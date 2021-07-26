3dd2e8cd97e9130b38349acd958abde975555e3f
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-07-26 10:31:26 +0900

    Update PackageInit.py

--- SuperMath/PackageInit.py
+++ SuperMath/PackageInit.py
@@ -1,44 +1,44 @@
-# ==================Trig======================
-import Trig as T
-
-def factorial(x):
-    return T.factorial(x)
-
-def eulerNumber(precision):
-    return T.eulerNumber(precision)
-
-def eulerNumber2(precision):
-    return T.eulerNumber2(precision)
-
-def eulerNumber3(precision):
-    return T.eulerNumber3(precision)
-
-def piF(x):
-    return T.piF(x)
-
-def summatePiF(limit):
-    return T.summatePiF(limit)
-
-def pi(precision):
-    return T.pi(precision)
-
-def pi2(precision):
-    return T.pi2(precision)
+# =================MaclauringSeries===============
+import MaclaurinSeries as M
 
 def summateExponent(x, precision):
-    return T.summateExponent(x, precision)
+    return M.summateExponent(x, precision)
 
 def exponentMaclaurin(x, precision):
-    return T.exponentMaclaurin(x, precision)
+    return M.exponentMaclaurin(x, precision)
 
 def summateFractional(x, precision):
-    return T.summateFractional(x, precision)
+    return M.summateFractional(x, precision)
 
 def fractional(x, precision): # return 1/(1-x) in maclaurin series for |x| < 1
     return summateFractional(x, precision) # when x = 1, undefined
 
-def f(x, n):
-    return T.f(x, n)
+# ==================Constant====================
+import Constants as C
+
+def eulerNumber(precision):
+    return C.eulerNumber(precision)
+
+def eulerNumber2(precision):
+    return C.eulerNumber2(precision)
+
+def eulerNumber3(precision):
+    return C.eulerNumber3(precision)
+
+def piF(x):
+    return C.piF(x)
+
+def summatePiF(limit):
+    return C.summatePiF(limit)
+
+def pi(precision):
+    return C.pi(precision)
+
+def pi2(precision):
+    return C.pi2(precision)
+
+# ==================Trig======================
+import Trig as T
 
 def sinMod(x, n):
     return T.sin(x, n)
@@ -46,17 +46,17 @@ def sinMod(x, n):
 def sin(x):
     return sinMod(x, 84)
 
-def g(x, n):
-    return T.g(x, n)
-
 def cosMod(x, n):
     return T.cos(x, n)
 
 def cos(x):
     return cosMod(x, 84)
 
-def tan(x, n):
-    return sin(x, n)/cos(x, n)#maximum limit n = 84
+def tanMod(x, n):
+    return sinMod(x, n)/cosMod(x, n)#maximum limit n = 84
+
+def tan(x):
+    return tanMod(x, 84)
 
 # ==================EulerRule======================
 import EulerCalc as E
